67ed01a4826049d569e9a7515792d79509c9b0d1
Mylosz <miloszk1990@gmail.com>
2025-04-09 21:53:22 +0200

    Move column definitions into a separate file

---
+++ backend/analyser/src/csv_columns.py
@@ -0,0 +1,39 @@
+overall_parameters = ["Revs", "Speed", "ExternalTemp", "Datetime", "Time_Diff"]
+driving_parameters = ["Revs", "Speed", "InjFlow", "AccelPedalPos"]
+engine_parameters = [
+    "Battery",
+    "Coolant",
+    "Datetime",
+    "Errors",
+    "OilCarbon",
+    "OilDilution",
+    "OilTemp",
+    "Revs",
+]
+fap_parameters = [
+    "Avg10regen",
+    "FAP life",
+    "FAPAdditiveRemain",
+    "FAPAdditiveVol",
+    "FAPcinder",
+    "FAPdeposits",
+    "FAPlifeLeft",
+    "FAPpressure",
+    "FAPsoot",
+    "FAPtemp",
+    "LastRegen",
+    "Revs",
+    "Speed",
+]
+fap_regen_parameters = [
+    "FAPpressure",
+    "FAPtemp",
+    "FAPsoot",
+    "Revs",
+    "Speed",
+    "InjFlow",
+    "REGEN",
+    "LastRegen",
+    "Datetime",
+    "Time_Diff",
+]
--- backend/analyser/src/main.py
+++ backend/analyser/src/main.py
@@ -2,6 +2,13 @@ from json import dumps
 from time import time
 
 import pandas as pd
+from csv_columns import (
+    driving_parameters,
+    engine_parameters,
+    fap_parameters,
+    fap_regen_parameters,
+    overall_parameters,
+)
 from driving_parameters import DrivingParameters
 from engine_parameters import EngineParameters
 from fap_parameters import FapParameters
@@ -11,6 +18,13 @@ from overall_parameters import OverallParameters
 
 class FapLogAnalyser:
     def __init__(self, file_path):
+        self.all_columns = set(
+            driving_parameters
+            + engine_parameters
+            + fap_parameters
+            + fap_regen_parameters
+            + overall_parameters
+        )
         self.csv = pd.read_csv(file_path, delimiter=";", encoding="latin1")
         self._process_data()
         self.result = self._analyse_parameters()
@@ -23,31 +37,7 @@ class FapLogAnalyser:
 
     def _process_data(self):
         """Preprocess the data."""
-        numeric_columns = [
-            "AccelPedalPos",
-            "Avg10regen",
-            "Battery",
-            "Coolant",
-            "Errors",
-            "ExternalTemp",
-            "FAP life",
-            "FAPAdditiveRemain",
-            "FAPAdditiveVol",
-            "FAPcinder",
-            "FAPdeposits",
-            "FAPlifeLeft",
-            "FAPpressure",
-            "FAPsoot",
-            "FAPtemp",
-            "InjFlow",
-            "LastRegen",
-            "OilCarbon",
-            "OilDilution",
-            "OilTemp",
-            "REGEN",
-            "Revs",
-            "Speed",
-        ]
+        numeric_columns = list(set(self.all_columns) - {"Datetime", "Time_Diff"})
         for col in numeric_columns:
             if col in self.csv.columns:
                 self.csv[col] = pd.to_numeric(self.csv[col], errors="coerce")
@@ -60,53 +50,18 @@ class FapLogAnalyser:
 
     def _analyse_parameters(self):
         """Preprocess the data."""
-        overall_parameters = ["Revs", "Speed", "ExternalTemp", "Datetime", "Time_Diff"]
-        driving_parameters = ["Revs", "Speed", "InjFlow", "AccelPedalPos"]
-        engine_parameters = [
-            "Battery",
-            "Coolant",
-            "Datetime",
-            "Errors",
-            "OilCarbon",
-            "OilDilution",
-            "OilTemp",
-            "Revs",
-        ]
-        fap_parameters = [
-            "Avg10regen",
-            "FAP life",
-            "FAPAdditiveRemain",
-            "FAPAdditiveVol",
-            "FAPcinder",
-            "FAPdeposits",
-            "FAPlifeLeft",
-            "FAPpressure",
-            "FAPsoot",
-            "FAPtemp",
-            "LastRegen",
-            "Revs",
-            "Speed",
-        ]
-        fap_regen_parameters = [
-            "FAPpressure",
-            "FAPtemp",
-            "FAPsoot",
-            "Revs",
-            "Speed",
-            "InjFlow",
-            "REGEN",
-            "LastRegen",
-            "Datetime",
-            "Time_Diff",
-        ]
+        csv_columns = self.csv.columns
+        overall_columns = list(set(csv_columns) & set(overall_parameters))
+        driving_columns = list(set(csv_columns) & set(driving_parameters))
+        engine_columns = list(set(csv_columns) & set(engine_parameters))
+        fap_columns = list(set(csv_columns) & set(fap_parameters))
+        fap_regen_columns = list(set(csv_columns) & set(fap_regen_parameters))
         return {
-            "overall": OverallParameters(self.csv[overall_parameters].copy()).result,
-            "driving": DrivingParameters(self.csv[driving_parameters].copy()).result,
-            "engine": EngineParameters(self.csv[engine_parameters].copy()).result,
-            "fap": FapParameters(self.csv[fap_parameters].copy()).result,
-            "fapRegen": FapRegenParameters(
-                self.csv[fap_regen_parameters].copy()
-            ).result,
+            "overall": OverallParameters(self.csv[overall_columns].copy()).result,
+            "driving": DrivingParameters(self.csv[driving_columns].copy()).result,
+            "engine": EngineParameters(self.csv[engine_columns].copy()).result,
+            "fap": FapParameters(self.csv[fap_columns].copy()).result,
+            "fapRegen": FapRegenParameters(self.csv[fap_regen_columns].copy()).result,
         }
 
 
