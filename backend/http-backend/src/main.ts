036fe282805088568980dd72d5cc0fe42310d99d
Mylosz <miloszk1990@gmail.com>
2025-04-15 21:54:44 +0200

    Add NATS microservice connection in main.ts to receive NATS messages

--- backend/http-backend/src/main.ts
+++ backend/http-backend/src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app.port') || 3000;
+  const natsUrl =
+    configService.get<string>('nats.url') || 'nats://localhost:4222';
+
+  app.connectMicroservice({
+    transport: Transport.NATS,
+    options: {
+      servers: [natsUrl],
+    },
+  });
+
+  await app.startAllMicroservices();
 
   const config = new DocumentBuilder()
     .setTitle('FAP Analysis API')
