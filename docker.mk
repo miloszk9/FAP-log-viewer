8adcff67d76dc08fa38c45b5e5622658b2393466
Mylosz <miloszk1990@gmail.com>
2025-04-29 22:02:22 +0200

    Refactor Docker build process, add docker build for ARM

--- docker.mk
+++ docker.mk
@@ -4,29 +4,22 @@
 # Docker configuration
 DOCKER_IMAGE_NAME ?= $(error DOCKER_IMAGE_NAME must be set)
 DOCKER_REGISTRY_LOCAL ?= localhost:5000
-DOCKER_DEFAULT_TAG ?= latest
 
-# Build Docker image
-# Usage: make docker-build
-docker-build:
-	docker build -t $(DOCKER_IMAGE_NAME):$(DOCKER_DEFAULT_TAG) .
-
-# Push Docker image
+# Build and push Docker image to remote registry
 # Usage: make docker-push tag=TAG
-docker-push:
+docker-prod:
 	@if [ -z "$(tag)" ]; then \
 		echo "Error: Tag is required. Usage: make docker-push tag=TAG"; \
 		exit 1; \
 	fi
-	docker tag $(DOCKER_IMAGE_NAME):$(DOCKER_DEFAULT_TAG) $(DOCKER_IMAGE_NAME):$(tag)
-	docker push $(DOCKER_IMAGE_NAME):$(tag)
+	docker buildx build --platform linux/amd64,linux/arm64 -t $(DOCKER_IMAGE_NAME):$(tag) --push .
 
-# Push Docker image to local registry
+# Build and push Docker image to local registry
 # Usage: make docker-push-local tag=TAG
-docker-push-local:
+docker-local:
 	@if [ -z "$(tag)" ]; then \
 		echo "Error: Tag is required. Usage: make docker-push-local tag=TAG"; \
 		exit 1; \
 	fi
-	docker tag $(DOCKER_IMAGE_NAME):$(DOCKER_DEFAULT_TAG) $(DOCKER_REGISTRY_LOCAL)/$(DOCKER_IMAGE_NAME):$(tag)
+	docker build -t $(DOCKER_IMAGE_NAME):$(tag) .
 	docker push $(DOCKER_REGISTRY_LOCAL)/$(DOCKER_IMAGE_NAME):$(tag) 
