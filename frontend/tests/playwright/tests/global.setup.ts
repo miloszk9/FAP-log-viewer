8c27b9970ad01f56996df06e93009b5d38231910
Mylosz <miloszk1990@gmail.com>
2025-11-08 14:35:35 +0100

    Add docker-compose configuration for E2E testing and update Playwright setup/teardown scripts to use the new file

--- frontend/tests/playwright/tests/global.setup.ts
+++ frontend/tests/playwright/tests/global.setup.ts
@@ -2,5 +2,5 @@ import { test as setup } from "@playwright/test";
 import { execSync } from "child_process";
 
 setup("setup docker", async () => {
-  execSync("cd ../ && docker compose up -d --build", { stdio: "inherit" });
+  execSync("cd ../ && docker compose -f docker-compose-e2e.yml up -d --build", { stdio: "inherit" });
 });
--- frontend/tests/playwright/tests/global.teardown.ts
+++ frontend/tests/playwright/tests/global.teardown.ts
@@ -2,5 +2,5 @@ import { test as teardown } from "@playwright/test";
 import { execSync } from "child_process";
 
 teardown("teardown docker", async () => {
-  execSync("cd ../ && docker compose down -v", { stdio: "inherit" });
+  execSync("cd ../ && docker compose -f docker-compose-e2e.yml down -v", { stdio: "inherit" });
 });
