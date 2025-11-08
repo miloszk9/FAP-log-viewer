08646e5723cc7eed406a8c1c3ae3d702d577e319
Mylosz <miloszk1990@gmail.com>
2025-11-08 14:52:41 +0100

    Update Playwright configuration: set base URL, adjust retry settings, and enhance timeout options for tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const APP_BASE_URL = "http://localhost:4321";
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -18,18 +20,19 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  expect: {
+    timeout: 5_000,
+  },
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: APP_BASE_URL,
     trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
@@ -37,7 +40,6 @@ export default defineConfig({
     {
       name: "setup",
       testMatch: /global\.setup\.ts/,
-      teardown: "teardown",
     },
     {
       name: "teardown",
@@ -48,16 +50,16 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
       dependencies: ["setup"],
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-      dependencies: ["setup"],
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-      dependencies: ["setup"],
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    //   dependencies: ["setup"],
+    // },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    //   dependencies: ["setup"],
+    // },
 
     /* Test against mobile viewports. */
     // {
